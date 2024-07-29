40c11b0ae76e1c3edbc33976189ba3fd0badb100
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2024-07-29 20:56:48 +0530

    Delete 29.07.2024/cap without function.py

--- 29.07.2024/cap without function.py
+++
@@ -1,9 +0,0 @@
-s=input("Enter the sentence : ")
-l=s.split()
-t=[]
-for i in l:
-        if i[0].islower():
-            t.append(i[0].upper()+i[1:])
-        else:
-            t.append(i)
-print(" ".join(t))
