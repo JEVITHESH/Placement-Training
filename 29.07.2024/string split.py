3ec178cf9e107caac8cbdb149e008e80466e0519
JEVITHESH <151340536+JEVITHESH@users.noreply.github.com>
2024-07-29 20:58:26 +0530

    Delete 29.07.2024/string split.py

--- 29.07.2024/string split.py
+++
@@ -1,8 +0,0 @@
-def split_and_join(line):
-    s = line.split(" ")
-    result = '-'.join(s)
-    return result
-if __name__ == '__main__':
-    line = input()
-    result = split_and_join(line)
-    print(result)
